528a43539fe85d0c076fd878e34a8a245fbbe661
Mikah Mathews <mikah@outofband.org>
2020-04-01 09:34:34 -0700

    It will add complete back end logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 
 
 var leapYear = function(year) {
-  if ((year % 100 === 0) && (year % 4 === 0)){
+  if ((year % 100 === 0) && (year % 4 === 0) || (year % 400 === 0)){
     return true;
   }  else {
     return false;
